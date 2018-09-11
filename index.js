d1b192ce601ef2d2b054850a012cd0a242dca69a
Arthur Edelstein <arthuredelstein@gmail.com>
2018-09-10 22:27:17 -0700

    clean up

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const ioHook = require('iohook');
 const robot = require("robotjs");
 const { KEYS } = require("./keyconstants.js");
 
-console.log(KEYS);
-
 const BUTTONS = [null, "left", "middle", "right"];
 
 let sleep = timeMs => new Promise(resolve => setTimeout(resolve, timeMs));
@@ -14,21 +12,23 @@ let recordSeries = () => new Promise(resolve => {
   for (let type of ["mousedown", "mousedrag", "mouseup", "mousemove", "mousewheel", "keydown", "keyup"]) {
     ioHook.on(type, event => {
       event['timeStamp'] = Date.now() - startTime;
-      if (type === "keydown`") {
-        console.log(event["keycode"]);
-      }
       eventSeries.push(event);
     });
   }
-  ioHook.registerShortcut([29, 57], () => {
+  let shortcutId = ioHook.registerShortcut([29, 57], () => {
     ioHook.stop();
     resolve(eventSeries);
+    ioHook.unregisterShortcut(shortcutId);
   });
   ioHook.start();
 });
 
+let robotKeyCode = (ioHookKeyCode) =>
+    ioHookKeyCode && KEYS[ioHookKeyCode.toString()];
+
 let playback = async (eventSeries) => {
   let startTime = Date.now();
+  let downKeys = new Set();
   for (let event of eventSeries) {
     let delta = event.timeStamp  - (Date.now() - startTime);
     if (!((event.type === "mousemove" ||
@@ -49,49 +49,33 @@ let playback = async (eventSeries) => {
         robot.mouseToggle("up", BUTTONS[event.button]);
         break;
       case "mousewheel":
-        console.log(event);
         break;
       case "keydown":
-//        robot.keyToggle("down");
+        {
+          let code = robotKeyCode(event.keycode);
+          if (code) {
+            robot.keyToggle(code, "down");
+            downKeys.add(code);
+          }
+        }
         break;
       case "keyup":
-//        robot.keyToggle("up");
-        break;
+        {
+          let code = robotKeyCode(event.keycode);
+          if (code) {
+            robot.keyToggle(code, "up");
+            downKeys.delete(code);
+          }
+        }
       }
+    }
+    for (let code of downKeys) {
+      robot.keyToggle(code, "up");
     }
   }
 };
 
 (async () => {
   let series = await recordSeries();
-//  console.log(JSON.stringify(series));
   await playback(series);
 })();
-
-
-
-/*
-// Speed up the mouse.
-robot.setMouseDelay(2);
-
-var twoPI = Math.PI * 2.0;
-var screenSize = robot.getScreenSize();
-var height = (screenSize.height / 2) - 10;
-var width = screenSize.width;
-
-for (var x = 0; x < width; x++)
-{
-  y = height * Math.sin((twoPI * x) / width) + height;
-  robot.moveMouse(x, y);
-}
-*/
-/*const screenshot = require('screenshot-desktop');
-
-(async () => {
-  try {
-    await screenshot();
-  } catch (e) {
-    console.log(e);
-  }
-})();
-*/
