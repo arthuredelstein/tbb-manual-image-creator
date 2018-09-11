77a6874d2c01755b4b689f34cb14d1663ed3239b
Arthur Edelstein <arthuredelstein@gmail.com>
2018-09-11 15:29:26 -0700

    get langpack downloads

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const robot = require("robotjs");
 const Jimp = require('jimp');
+const request = require('request-promise-native');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const { JSDOM } = require("jsdom");
 
 const { recordInput, playbackInput } = require("./user-input-repeater.js");
 
@@ -20,10 +24,37 @@ let writeImageBuffer = (file, {buffer, width, height, bufferFormat }) =>
 
 let sleep = timeMs => new Promise(resolve => setTimeout(resolve, timeMs));
 
+let tor_browser_dir = "/home/arthur/Downloads/tor-browser_en-US";
+
+let get_firefox_version = async (path) => {
+  let { stdout, stderr } = await exec(`${path}/Browser/firefox --version`);
+  if (stderr) {
+    throw new Error(stderr);
+  }
+  return stdout.match(/Mozilla Firefox (\S+)/)[1];
+};
+
+let langpack_urls = async (version) => {
+  let baseUrl = `https://ftp.mozilla.org/pub/firefox/releases/${version}esr/linux-x86_64/xpi/`;
+  let indexPage = await request(baseUrl);
+  let dom = new JSDOM(indexPage);
+  let items = [...dom.window.document.querySelectorAll("table tr td a")]
+      .map(element => element.innerHTML)
+      .filter(text => text !== "..")
+      .map(stem => baseUrl + stem);
+  return items;
+};
+
 (async () => {
-  let series = await recordInput();
+  let version = await get_firefox_version(tor_browser_dir);
+  console.log(version);
+  let result = await langpack_urls(version);
+  console.log(result);
+//  console.log(result);
+/*  let series = await recordInput();
   await playbackInput(series);
   let { image, width, height } = robot.screen.capture();
   await writeImageBuffer("test2.png", { buffer: image, width, height,
-                                        bufferFormat: "bgra"});
+  bufferFormat: "bgra"});*/
+  
 })();
