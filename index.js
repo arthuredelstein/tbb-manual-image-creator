399ee79c0d3d8706befc4f86f0e109d10f627d98
Arthur Edelstein <arthuredelstein@gmail.com>
2018-09-11 12:16:53 -0700

    refactor and use correct colors

--- index.js
+++ index.js
@@ -1,88 +1,29 @@
-const ioHook = require('iohook');
 const robot = require("robotjs");
 const Jimp = require('jimp');
 
-const { KEYS } = require("./keyconstants.js");
+const { recordInput, playbackInput } = require("./user-input-repeater.js");
 
-const BUTTONS = [null, "left", "middle", "right"];
+let writeImageBuffer = (file, {buffer, width, height, bufferFormat }) =>
+  new Promise((resolve, reject) => {
+    let buffer2 = new Uint8Array(buffer);
+    if (bufferFormat.toLowerCase() === "bgra") {
+      // Jimp needs rgba:
+      for (let i = 0; i < buffer2.length; i+=4) {
+        buffer2[i] = buffer[i+2];
+        buffer2[i+2] = buffer[i];
+      }
+    }
+    jimp = new Jimp(1, 1);
+    jimp.bitmap = { data: buffer2, width, height };
+    jimp.write(file, resolve);
+  });
 
 let sleep = timeMs => new Promise(resolve => setTimeout(resolve, timeMs));
 
-let recordSeries = () => new Promise(resolve => {
-  let eventSeries = [];
-  let startTime = Date.now();
-  for (let type of ["mousedown", "mousedrag", "mouseup", "mousemove", "mousewheel", "keydown", "keyup"]) {
-    ioHook.on(type, event => {
-      event['timeStamp'] = Date.now() - startTime;
-      eventSeries.push(event);
-    });
-  }
-  let shortcutId = ioHook.registerShortcut([29, 57], () => {
-    ioHook.stop();
-    resolve(eventSeries);
-    ioHook.unregisterShortcut(shortcutId);
-  });
-  ioHook.start();
-});
-
-let robotKeyCode = (ioHookKeyCode) =>
-    ioHookKeyCode && KEYS[ioHookKeyCode.toString()];
-
-let playback = async (eventSeries) => {
-  let startTime = Date.now();
-  let downKeys = new Set();
-  for (let event of eventSeries) {
-    let delta = event.timeStamp  - (Date.now() - startTime);
-    if (!((event.type === "mousemove" ||
-           event.type === "mousedrag") &&
-          event.type === delta < 0)) {
-      await sleep(delta);
-      switch (event.type) {
-      case "mousemove":
-        robot.moveMouse(event.x, event.y);
-        break;
-      case "mousedrag":
-        robot.dragMouse(event.x, event.y);
-        break;
-      case "mousedown":
-        robot.mouseToggle("down", BUTTONS[event.button]);
-        break;
-      case "mouseup":
-        robot.mouseToggle("up", BUTTONS[event.button]);
-        break;
-      case "mousewheel":
-        break;
-      case "keydown":
-        {
-          let code = robotKeyCode(event.keycode);
-          if (code) {
-            robot.keyToggle(code, "down");
-            downKeys.add(code);
-          }
-        }
-        break;
-      case "keyup":
-        {
-          let code = robotKeyCode(event.keycode);
-          if (code) {
-            robot.keyToggle(code, "up");
-            downKeys.delete(code);
-          }
-        }
-      }
-    }
-    // Make sure all keys are up at the end.
-    for (let code of downKeys) {
-      robot.keyToggle(code, "up");
-    }
-  }
-};
-
 (async () => {
-  let series = await recordSeries();
-  await playback(series);
-  let { image, width, height, bitsPerPixel } = robot.screen.capture();
-  jimp = new Jimp(10, 10);
-  jimp.bitmap = {data: image, width, height };
-  jimp.write("test1.png");
+  let series = await recordInput();
+  await playbackInput(series);
+  let { image, width, height } = robot.screen.capture();
+  await writeImageBuffer("test2.png", { buffer: image, width, height,
+                                        bufferFormat: "bgra"});
 })();
