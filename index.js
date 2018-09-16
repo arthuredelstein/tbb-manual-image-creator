8f858c5504f081ac74291379c428b2956e39d225
Arthur Edelstein <arthuredelstein@gmail.com>
2018-09-15 23:16:46 -0700

    download langpacks and run over multiple locales

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const util = require('util');
 const child_process = require('child_process');
 const spawn = child_process.spawn;
 const exec = util.promisify(require('child_process').exec);
-const { promises: fs } = require('fs');
+const fs = require('fs');
+const fsAsync = fs.promises;
+const path = require('path');
+const mkdirp = require('mkdirp-promise');
 const { JSDOM } = require("jsdom");
 
 const { recordInput, playbackInput } = require("./user-input-repeater.js");
@@ -48,27 +51,66 @@ let langpack_urls = async (version) => {
   return items;
 };
 
+let downloadFile = (url, diskPath) => new Promise(
+  resolve => request(url)
+    .pipe(fs.createWriteStream(diskPath))
+    .on("finish", resolve));
+
+let download_langpacks = async (version) => {
+  let xpiDir = `${tor_browser_dir}/Browser/TorBrowser/Data/Browser/profile.default/extensions`;
+  let langpacks = await langpack_urls(version);
+  for (let langpack of langpacks) {
+    let remoteFileName = path.basename(langpack);
+    let locale = remoteFileName.split(".")[0];
+    let destFile = `${xpiDir}/langpack-${locale}@firefox.mozilla.org.xpi`;
+    if (!fs.existsSync(destFile)) {
+      console.log(`downloading ${langpack}`);
+      await downloadFile(langpack, `${xpiDir}/langpack-${locale}@firefox.mozilla.org.xpi`);
+    }
+  }
+  console.log("langpacks are fully downloaded");
+};
+
 let start_tor_browser = async (tor_browser_dir, locale) => {
   let profileDir = `${tor_browser_dir}/Browser/TorBrowser/Data/Browser/profile.default/`;
   console.log(profileDir);
   // Wipe custom prefs each time to make behavior reproducible.
-  await fs.unlink(profileDir + "prefs.js");
-  await fs.writeFile(profileDir + "user.js", `user_pref('intl.locale.requested', '${locale}');\n`);
+  await fsAsync.unlink(profileDir + "prefs.js");
+  await fsAsync.writeFile(profileDir + "user.js", `user_pref('intl.locale.requested', '${locale}');\n`);
   return spawn(`${tor_browser_dir}/Browser/firefox`);
+};
+
+let captureScreenToFile = async (filename) => {
+  let { image, width, height } = robot.screen.capture();
+  await writeImageBuffer(filename,
+                         { buffer: image, bufferFormat: "bgra",
+                           width, height });
+};
+
+let acquireImagesForLocales = async ({inputSeries, imageName,
+                                         locales, imageDir}) => {
+  for (let locale of locales) {
+    let browserProcess2 = await start_tor_browser(tor_browser_dir, locale);
+    await playbackInput(inputSeries);
+    await mkdirp(`${imageDir}/${locale}/`);
+    captureScreenToFile(`${imageDir}/${locale}/${imageName}.png`);
+    browserProcess2.kill();
+  }
+};
+
+let recordInputSeries = async () => {
+  let browserProcess = await start_tor_browser(tor_browser_dir, "en-US");
+  let series = await recordInput();
+  browserProcess.kill();
+  return series;
 };
 
 (async () => {
   let version = await get_firefox_version(tor_browser_dir);
-  console.log(version);
-  console.log(await langpack_urls(version));
-  let browserProcess = await start_tor_browser(tor_browser_dir, "en-US");
-  let series = await recordInput();
-  browserProcess.kill();
-  let browserProcess2 = await start_tor_browser(tor_browser_dir, "de-DE");
-  await playbackInput(series);
-  let { image, width, height } = robot.screen.capture();
-  await writeImageBuffer("browser_screenshot.png",
-                         { buffer: image, bufferFormat: "bgra",
-                           width, height });
-  browserProcess2.kill();
+  await download_langpacks(version);
+  let inputSeries = await recordInputSeries();
+  let locales = ["en-US", "de-DE", "fr-FR", "es-ES"];
+  await acquireImagesForLocales(
+    {inputSeries, imageName: "captcha", locales, imageDir: "images"});
+  console.log("done!");
 })();
